d3aa3ae25b4cd5aeec1f80934e485592d5133dfe
d.naumov <d.naumov@general-soft.by>
2019-03-04 14:42:47 +0300

    Delete images from Medic template

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,13 +12,13 @@ let mix = require('laravel-mix');
  */
 mix.js('resources/assets/js/app.js', 'public/vali-admin/js');
 
-mix.styles([
-        'resources/assets/css/main_theme.css',
-    ],
-    'public/main_theme/css/style.css');
-
-mix.scripts([
-        'resources/assets/js/main_theme/scripts.js',
-        'resources/assets/js/main_theme/datepicker-ru.js',
-    ],
-    'public/main_theme/js/scripts.js');
+// mix.styles([
+//         'resources/assets/css/main_theme.css',
+//     ],
+//     'public/main_theme/css/style.css');
+//
+// mix.scripts([
+//         'resources/assets/js/main_theme/scripts.js',
+//         'resources/assets/js/main_theme/datepicker-ru.js',
+//     ],
+//     'public/main_theme/js/scripts.js');
