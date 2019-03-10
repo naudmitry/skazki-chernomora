eee20b40563030ba7dcab38cab5f29598150b91e
Naumov Dmitry <naumovdmitry@outlook.com>
2019-03-10 22:38:34 +0300

    Fix widget blog

--- resources/views/miracle/widgets/Blog/front.blade.php
+++ resources/views/miracle/widgets/Blog/front.blade.php
@@ -10,22 +10,18 @@
                     <article class="post post-masonry">
                         <div class="post-image">
                             <div class="image">
-                                <img src="{{ $blog->link }}" alt="{{ $blog->title }}">
+                                <img class="widget-blog-img" src="{{ $blog->link }}" alt="{{ $blog->title }}">
                                 <div class="image-extras">
                                     <a href="{{ $blog->getRoute() }}" class="post-gallery"></a>
                                 </div>
                             </div>
                         </div>
                         <div class="post-content">
-                            <div class="post-author">
-                                <a href="#"><img src="http://placehold.it/100x100" alt=""></a>
-                            </div>
                             <div class="post-meta">
-                                <span class="entry-author fn">By <a href="#">Admin</a></span>
-                                <span class="entry-time"><span class="updated no-display">2014-09-09T15:57:08+00:00</span><span class="published">12 Nov, 2014</span></span>
-                                <span class="post-category">in <a href="#">Web Design</a></span>
+                                <span class="entry-author fn">{{ $blog->author->full_name }}</span>
+                                <span class="entry-time"><span class="updated no-display">{{ $blog->getFormatUpdatedAtAttribute() }}</span><span class="published">{{ $blog->getFormatCreatedAtAttribute() }}</span></span>
                             </div>
-                            <h3 class="entry-title"><a href="#">{{ mb_strimwidth($blog->name, 0, 24, "...") }}</a></h3>
+                            <h3 class="entry-title"><a href="{{ $blog->getRoute() }}">{{ mb_strimwidth($blog->name, 0, 24, "...") }}</a></h3>
                             <p>{!! mb_strimwidth(strip_tags($blog->content), 0, 110, "...") !!}</p>
                         </div>
                         <div class="post-action">
