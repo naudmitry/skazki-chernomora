3a758ceba8b07fa89e314746f9a3a97ad80da856
d.naumov <d.naumov@general-soft.by>
2019-03-11 14:39:53 +0300

    Fix recent post widget

--- resources/views/miracle/widgets/PostWrapper/front.blade.php
+++ resources/views/miracle/widgets/PostWrapper/front.blade.php
@@ -2,7 +2,7 @@
     <div class="owl-carousel post-slider style6" data-items="4" data-itemsPerDisplayWidth="[[0, 1], [480, 1], [768, 2], [992, 3], [1200, 4]]">
         @foreach(data_get($setting, 'items', []) as $item)
             <article class="post">
-                <figure><img alt="" src="{{ $item->image_link }}"></figure>
+                <figure><img alt="{{ $item->title }}" src="{{ $item->image_link }}"></figure>
                 <div class="portfolio-hover-holder">
                     <div class="portfolio-text">
                         <div class="portfolio-text-inner">
@@ -11,7 +11,7 @@
                     </div>
                     <span class="portfolio-action">
                         <a href="{{ $item->image_link }}" class="soap-mfp-popup"><i class="fa fa-eye has-circle"></i></a>
-                        <a href="portfolio-single1.html"><i class="fa fa-chain has-circle"></i></a>
+                        <a href="#"><i class="fa fa-chain has-circle"></i></a>
                     </span>
                 </div>
             </article>
