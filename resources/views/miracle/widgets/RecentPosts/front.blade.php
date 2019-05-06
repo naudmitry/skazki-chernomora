46fb214f35a928fe26ab18124b9d43552c2c6a52
Naumov Dmitry <naumovdmitry@outlook.com>
2019-05-06 22:44:45 +0300

    fixes

--- resources/views/miracle/widgets/RecentPosts/front.blade.php
+++ resources/views/miracle/widgets/RecentPosts/front.blade.php
@@ -6,7 +6,7 @@
                 <a href="#" class="post-author-avatar"><span><img class="widget-recent-post-img" src="{{ $blog->link }}" alt="{{ $blog->title }}"></span></a>
                 <div class="post-content">
                     <a href="{{ $blog->getRoute() }}" class="post-title">{{ mb_strimwidth($blog->name, 0, 27, "...") }}</a>
-                    <p class="post-meta">{{ $blog->author->full_name }} . {{ strftime('%d %B %G', strtotime($blog->created_at)) }}</p>
+                    <p class="post-meta">{{ $blog->author->full_name ?? '' }} . {{ strftime('%d %B %G', strtotime($blog->created_at)) }}</p>
                 </div>
             </li>
         @endforeach
