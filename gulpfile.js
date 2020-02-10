76aa99ee8f335dd344e6fa51ba93cc77110ed045
Naumov Dmitry <naumovdmitry@outlook.com>
2020-02-10 08:40:00 +0300

    Fix

--- gulpfile.js
+++
@@ -1,9 +0,0 @@
-// const elixir = require('laravel-elixir');
-//
-// require('laravel-elixir-vue');
-//
-// elixir(mix => {
-//     mix.sass('app.scss')
-//         .webpack('app.js')
-//         .version(['js/app.js']);
-// });
