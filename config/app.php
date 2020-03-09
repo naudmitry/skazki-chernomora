afe7463eeb71e3644e2d80b91e5dc1d3a12e2585
Dmitry Naumov <d.naumov@general-soft.by>
2020-03-09 15:41:53 +0300

    Deleted IDE Helper

--- config/app.php
+++ config/app.php
@@ -163,7 +163,6 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
 
         // Widgets
         // https://github.com/arrilot/laravel-widgets
